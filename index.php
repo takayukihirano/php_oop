caafba90e07bb661a8a3a64713f12785a92f51f9
平野貴之 <>
2019-10-29 09:50:34 +0800

    create.php作成　ヘッダー追加　フォーム追加
    create.php 作成済み　ヘッダー追加　フォーム追加

--- index.php
+++ index.php
@@ -5,11 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="asset/css/reset.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+    <header class="px-5 bg-primary">
+        <nav class="navbar navbar-dark">
+            <a href="index.php" class="navbar-brand">TODO APP</a>
+            <div class="justify-content-end">
+                <span class="text-light">
+                    SeedKun
+                </span>
+            </div>
+        </nav>
+    </header>
+    <main class="container py-5">
+        <section>
+            <form class="form-row justify-content-center" action="create.php" method="POST">
+                <div class="col-10 col-md-6 py-2">
+                    <input type="text" class="form-control" placeholder="ADD TODO" name="task">
+                </div>
+                <div class="py-2 col-md-3 col-10">
+                    <button type="submit" class="col-12 btn btn-primary">ADD</button>
+                </div>
+            </form>
+        </section>
+        <section>
+        </section>
+    </main>
 
 </body>
 </html>
