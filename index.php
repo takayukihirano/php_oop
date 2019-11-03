730b6b85da1b2eced922d22a558370ebc6964dfd
平野貴之 <>
2019-11-03 15:26:00 +0800

    due dateの作成　優先順位の作成とそれに伴うcssの作成
    元々あったdue_dateは作成日を表示したものだったので名前をdue_dateからcreated_dateに変更。それに伴いdbの方でも変更。
    todoの期日を表示するために別のdue_dateを作成するために日付を入力する機能を追加。
    そのタスクの優先順位を表示する機能を作るためにpriorityという新しい入力欄を作成しようとしたところでcssがindex.phpに反映されず一時停止。

--- index.php
+++ index.php
@@ -25,7 +25,13 @@
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <link rel="stylesheet" href="assets/css/style.css">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
 </head>
 <body>
     <header class="px-5 bg-primary">
@@ -33,17 +39,44 @@
             <a href="index.php" class="navbar-brand">TODO APP</a>
             <div class="justify-content-end">
                 <span class="text-light">
-                    Lovely
+                    Taka
                 </span>
             </div>
         </nav>
     </header>
-    <main class="container py-5">
-        <section>
-            <form class="form-row justify-content-center" action="create.php" method="POST">
-                <div class="col-10 col-md-6 py-2">
-                    <input type="text" class="form-control" placeholder="ADD TODO" name="task">
-                </div>
+
+<main class="container py-5">
+    <section>
+        <form class="form-row justify-content-center" action="create.php" method="POST">
+
+        <!-- todoの作成 -->
+        <div class="col-10 col-md-6 py-2">
+            <input type="text" class="form-control" placeholder="ADD TODO" name="task">
+        </div>
+
+        <!-- due dateの作成 -->
+        <div class="col-5 col-md-4 py-2">
+            <input type="date" class="form-control"  name="due_date">
+        </div>
+
+        <!-- 優先順位の設定 -->
+        <form type="get" action="#">
+            <div class="priority">
+                <input id="star1" type="radio" name="star" value="5" />
+                <label for="star1"><span class="text">最重要</span>★</label>
+                <input id="star2" type="radio" name="star" value="4" />
+                <label for="star2"><span class="text">かなり重要</span>★</label>
+                <input id="star3" type="radio" name="star" value="3" />
+                <label for="star3"><span class="text">まぁまぁ重要</span>★</label>
+                <input id="star4" type="radio" name="star" value="2" />
+                <label for="star4"><span class="text">あまり重要ではない</span>★</label>
+                <input id="star5" type="radio" name="star" value="1" />
+                <label for="star5"><span class="text">重要ではない</span>★</label>
+            </div>
+        </form>　       　
+
+
+
                 <div class="py-2 col-md-3 col-10">
                     <button type="submit" class="col-12 btn btn-primary">ADD</button>
                 </div>
@@ -54,7 +87,7 @@
               <thead>
                 <tr class="bg-primary text-light">
                     <th class=>TODO</th>
-                    <th>DUE DATE</th>
+                    <th>CREATED DATE</th>
                     <th>STATUS</th>
                     <th></th>
                     <th></th>
@@ -64,13 +97,13 @@
             <?php foreach ($tasks as $task): ?>
             <tr>
             <td><?php echo h($task['name']); ?></td>
-            <td><?php echo h($task['due_date']); ?></td>
+            <td><?php echo date("Y/m/d" , strtotime($task['created_date'])); ?></td>
             <td>NOT YET</td>
             <td>
-            <a class="text-success" href="edit.php?id=<?php echo h($task['id']);?>">EDIT</a>
+            <a class="text-success" href="edit.php?id=<?php echo h($task['id']);?>"><i class="material-icons">edit</i></a>
             </td>
             <td>
-            <a class="text-danger" href="delete.php?id=<?php echo h($task['id']);?>">DELETE</a>
+            <a class="text-danger" href="delete.php?id=<?php echo h($task['id']);?>"><i class="fas fa-trash-alt"></i></a>
             </td>
 
             </tr>
